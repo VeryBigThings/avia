5202be65cf702f47927a76c9036fd5946ac60922
ademirovic <azra.demirovic@verybigthings.com>
2020-10-18 21:07:33 +0200

    Add migrations

--- rel/bin/migrate_all.sh
+++ rel/bin/migrate_all.sh
@@ -3,18 +3,20 @@
 
 set +e
 
-while true; do
-  nodetool ping
-  EXIT_CODE=$?
+# while true; do
+#   nodetool ping
+#   EXIT_CODE=$?
 
-  if [ $EXIT_CODE -eq 0 ]; then
-    echo "Application is up!"
-    break
-  fi
-done
+#   if [ $EXIT_CODE -eq 0 ]; then
+#     echo "Application is up!"
+#     break
+#   fi
+# done
 
 set =e
 
 echo "Running migrations"
-release_ctl eval --mfa "Snitch.Tasks.ReleaseTasks.migrate/0" -- "$@"
+script_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+args="$@"
+$script_dir/nue eval "Snitch.Tasks.ReleaseTasks.migrate()"
 echo "Migrations run successfully"
